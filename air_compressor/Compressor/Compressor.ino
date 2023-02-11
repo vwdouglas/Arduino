ce988c46eee8daff7a824ec36a0af85475398e79
Doug <45984776+vwdouglas@users.noreply.github.com>
2023-02-11 17:32:36 -0600

    change pinouts for 4 chanel relay shield other bugs fixes

--- air_compressor/Compressor/Compressor.ino
+++ air_compressor/Compressor/Compressor.ino
@@ -1,22 +1,19 @@
 
-
-
-
-
 /*pressure 0 psi = 0 volts 
 //Pressure 150 psi = 5 Volts 
 turn off the compressure at 1010 ~ 140psi
 
 For Mitsubishi FR-700 inverter
-connect pin 2 to RL
-connect pin 3 to RM 
-connect pin 4 to RH
-Connect pin 5 to STF
+connect Relay 1 to RL    
+connect Relay 2 to RM  
+connect Relay 3 to RH 
+Connect Relay 5 to STF
 Connect SD to the Compressure side of the relays 
 
 Power Switch
-Connrct pin 6 to PSW
-connect GND to PSW
+Connect a switch from 5v to 10K res. to pin 10 for on. Pin 10 to gnd for off
+
+Pressure Sensor 5V, gnd, A1
 
 Display connect to arduino pins A4(SDA),A5(SCL) and 5V, Ground
 */
@@ -26,33 +23,38 @@ Display connect to arduino pins A4(SDA),A5(SCL) and 5V, Ground
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
-const int BUTTON_PIN = 6;
-
+const int swpin = 10;
+int lowSpeed = 7;  // Relay 1
+int medSpeed = 6;  // Relay 2
+int highSpeed = 5; // Relay 3
+int startFWD = 4;       // Relay 4
 int analogPin = A0;        // pressure sensor (middle terminal) connected to analog pin 1 outside leads to ground and +5V
 double inPutPressure = 0;  //Pressure gage
 double lowPressure = 95;
 double medPressure = 120;
-//int highPressure = 900;
 double offPressure = 140;
 double onPressure = 100;
 bool compressureState = true;  // true = on   false = off
 //bool compressureStateOn = true;
 float displayPressure = 0;
 float const multplyerForPSI = .1470588;
+int powerSW;   //power switch
 
 void setup() {
-  pinMode(6, INPUT_PULLUP);
-  pinMode(2, OUTPUT);  // sets the digital pin 2 as output Set freek drive on low
-  pinMode(3, OUTPUT);  // sets the digital pin 3 as output Set freek drive on med-low
-  pinMode(4, OUTPUT);  // sets the digital pin 4 as output Set freek drive on high
-  pinMode(5, OUTPUT);  // sets the digital pin 5 as output turn on freek drive.
+  pinMode(swpin, INPUT);//power button
+  pinMode(lowSpeed, OUTPUT);  // sets the digital pin 7 as output Set freek drive on low
+  pinMode(medSpeed, OUTPUT);  // sets the digital pin 6 as output Set freek drive on med-low
+  pinMode(highSpeed, OUTPUT);  // sets the digital pin 5 as output Set freek drive on high
+  pinMode(startFWD, OUTPUT);  // sets the digital pin 4 as output turn on freek drive.
 
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  inPutPressure = 0;
-
+  digitalWrite(lowSpeed, LOW);
+  digitalWrite(medSpeed, LOW);
+  digitalWrite(highSpeed, LOW);
+  digitalWrite(startFWD, HIGH);
+   powerSW = digitalRead(swpin);  //power switch
+    Serial.begin(9600);
+    Serial.print("button  ");
+    Serial.print(powerSW);
   // Display Pressure Section 150psi = 1020
   displayPressure = inPutPressure * 150 / 1020;
 
@@ -61,37 +63,8 @@ void setup() {
 
   lcd.backlight();
 }
-
-void loop() {
-  int powerSW = digitalRead(BUTTON_PIN);  //power switch
-
- 
-  // Power switch
-  //int powerSW = digitalRead(BUTTON_PIN);  //power switch
-  if (powerSW == LOW) {
-    // lcd.clear();
-    //lcd.setCursor(3, 3);
-    //lcd.print("POWER OFF");
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-  //Wiring Setup
-   // lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("D2=RL D3=RM D4=RH");
-    lcd.setCursor(1, 0);
-    lcd.print("D5=STF ");
-    lcd.setCursor(2, 0);
-    lcd.print("SD to all relays");
-    lcd.setCursor(3, 0);
-    lcd.print("PW~D6=PSW / GND=PSW");
-      
-        return;
-  }
-
-
-  // Display Pressure Section 150psi = 1020
+void displaySetup() {
+  // Display Pressure Section 150psi = kk
   lcd.clear();
   lcd.setCursor(0, 0);                // move cursor to   (0, 0)
   lcd.print("Doug's AirCompressor");  // print message at (0, 0)
@@ -103,7 +76,11 @@ void loop() {
   lcd.print(onPressure);
   lcd.setCursor(0, 3);  // move cursor to   (0, 2)
   lcd.print("Pressure ");
-  inPutPressure = analogRead(analogPin) * multplyerForPSI;
+  delay(500);
+}
+
+void displayWrite() {
+ 
   displayPressure = (inPutPressure);
   //lcd.setCursor(8, 3);  // move cursor to   (10, 3)
   lcd.print(displayPressure);
@@ -113,14 +90,54 @@ void loop() {
   //lcd.print("    ");
   delay(500);
 
-  // Compressure contral section
+}
 
+void powerOff () {
+ // Power switch
+  //int powerSW = digitalRead(BUTTON_PIN);  //power switch
+  
+    // lcd.clear();
+    //lcd.setCursor(3, 3);
+    //lcd.print("POWER OFF");
+    digitalWrite(lowSpeed, LOW);
+    digitalWrite(medSpeed, LOW);
+    digitalWrite(highSpeed, LOW);
+    digitalWrite(startFWD, LOW);
+  //Wiring Setup
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("D4=RL D7=RM D8=RH");
+    lcd.setCursor(0, 1);
+    lcd.print("D12=STF ");
+    lcd.setCursor(0, 2);
+    lcd.print("SD to all relays");
+    lcd.setCursor(0, 3);
+    lcd.print("PW~D6=PSW / GND=PSW");
+    delay(500);  
+   
+}
+
+void loop() {
+powerSW = digitalRead(swpin);  //power switch
+  Serial.println("button  ");
+Serial.print(powerSW);
+ if (powerSW == LOW) {
+  powerOff();
+   }
+ else if (powerSW == HIGH) {
+  displaySetup();
+ }
+ //while (!compressureState)  {
+  displayWrite();
+ 
+  // Compressure contral section
+  inPutPressure = analogRead(analogPin) * multplyerForPSI;
   if (inPutPressure >= offPressure) {
     compressureState = false;  // true = on "Low pressure"   false = off "High pressure"
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
+    digitalWrite(lowSpeed, LOW);
+    digitalWrite(medSpeed, LOW);
+    digitalWrite(highSpeed, LOW);
+    digitalWrite(startFWD, HIGH);
   }
   if (!compressureState) {  // true = on "Low pressure"   false = off "High pressure"
     if (inPutPressure <= onPressure) {
@@ -128,21 +145,36 @@ void loop() {
     }
   }
   if (compressureState) {
-    if (inPutPressure > medPressure) {  //LOw Pressure
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-    } else if (medPressure > inPutPressure && inPutPressure >= lowPressure) {  //Mid Pressure
-      digitalWrite(2, LOW);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, LOW);
-      digitalWrite(5, HIGH);
-    } else if (inPutPressure < lowPressure) {  //High Pressure (low pressure)
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
+    if (inPutPressure > medPressure) {  //High Pressure/ Low speed
+      digitalWrite(lowSpeed, HIGH);
+      digitalWrite(medSpeed, LOW);
+      digitalWrite(highSpeed, LOW);
+      digitalWrite(startFWD, HIGH);
+      Serial.print("High Pressure stf= ");
+      Serial.println(startFWD);
+      Serial.println();
+      delay(10);
+    } else if (medPressure > inPutPressure && inPutPressure >= lowPressure) {  //Mid Pressure / med speed
+      digitalWrite(lowSpeed, LOW);
+      digitalWrite(medSpeed, HIGH);
+      digitalWrite(highSpeed, LOW);
+      digitalWrite(startFWD, HIGH);
+      Serial.print("Med Pressure stf= ");
+      Serial.println(startFWD);
+      Serial.println();      
+      delay(500);
+    } else if (inPutPressure < lowPressure) {  //High Speed (low pressure)
+      digitalWrite(lowSpeed, LOW);
+      digitalWrite(medSpeed, LOW);
+      digitalWrite(highSpeed, HIGH);
+      digitalWrite(startFWD, HIGH);
+      delay(1000);
+      Serial.print("Low Pressure stf= ");
+      Serial.println(startFWD);
+      Serial.println();
     }
+
+
   }
 }
+//}
